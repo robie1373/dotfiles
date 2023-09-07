c405b1e9632bce17a3222e065dbb1a2b63df83d4
Robie <robie1373@gmail.com>
2023-09-07 08:45:45 -0400

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,37 +4,37 @@
 
 # start with xcode command line tools
 if [ ! -d "/Library/Developer/CommandLineTools/" ]; then
-  echo "/nAbout to install command line tools. This can take a while. Plug in?/n"
+  printf "/nAbout to install command line tools. This can take a while. Plug in?/n"
   xcode-select --install
 fi
 
 # Next get brew
 if [ ! -f "/opt/homebrew/bin/brew" ]; then
-  echo "/nInstalling Homebrew becasue it is missing.../n"
+  printf "/nInstalling Homebrew becasue it is missing.../n"
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
 
 # now install chezmoi
-echo "/nInstalling chezmoi/n"
+printf "/nInstalling chezmoi/n"
 brew install chezmoi
 # run chezmoi to get the bootstrap.sh This will also install all your other 
 # dotfiles.
 chezmoi init --apply --verbose https://github.com/robie1373/dotfiles.git
 
 # Once we have our Brewfile we can use that to install the rest
-if read -q "choice?Do you want to install the base bundle? (Recommended) [yN]"; then
+if read -q "choice?Do you want to install the base bundle? (Recommended) [yN]\n"; then
   brew bundle --file $HOME/Brewfile.base
 fi
 
-if read -q "choice?Do you want to install the coding bundle? [yN]"; then
+if read -q "choice?Do you want to install the coding bundle? [yN]\n"; then
   brew bundle --file $HOME/Brewfile.coding
 fi
 
-if read -q "choice?Do you want to install the hareware dev bundle? [yN]"; then
+if read -q "choice?Do you want to install the hareware dev bundle? [yN]\n"; then
   brew bundle --file $HOME/Brewfile.hardware
 fi
 # 1password 8 and the app store are having a hissy fit, so remind yourself
 # to install it manually like a troglodite.
-if read -q "choice?Do you need 1password? It has to be installed manually."; then
+if read -q "choice?Do you need 1password? It has to be installed manually. [yN]\n"; then
   open https://1password.com/downloads/mac/
 fi
